3e0abd93250241e454d1afee3874375edad0646f
Анненко Миша 3821Б1ПИмэ <146870367+jazz931@users.noreply.github.com>
2024-06-09 17:41:24 +0300

    Update fun.h

--- include/fun.h
+++ include/fun.h
@@ -2,8 +2,11 @@
 #ifndef INCLUDE_FUN_H_
 #define INCLUDE_FUN_H_
 
-unsigned int faStr1(const char *str);
-unsigned int faStr2(const char *str);
-unsigned int faStr3(const char *str);
+unsigned int faStr1(const char* str);
+unsigned int faStr2(const char* str);
+unsigned int faStr3(const char* str);
+
+unsigned int f1(const char* str);
+unsigned int f2(const char* str);
 
 #endif  // INCLUDE_FUN_H_
