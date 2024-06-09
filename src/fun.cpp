0db7b5d7c142081ef433ac62771c3530ce83764e
Анненко Миша 3821Б1ПИмэ <146870367+jazz931@users.noreply.github.com>
2024-06-09 17:31:14 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,79 +1,218 @@
 // Copyright 2022 UNN-IASR
-#include <ctype.h>
-#include <math.h>
-#include "fun.h"
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include "fun.h";
+
+std::vector<std::string> Return_replicated_words(const char* str) {
+    std::istringstream copy_str(str);
+    std::vector<std::string> push_words;
+    std::string word;
+    while (copy_str >> word) {
+        push_words.push_back(word);
+    }
+    return push_words;
+}
+
+bool checker_contain_number(std::string word) {
+    for (char item : word) {
+        if (item >= '0' && item <= '9') {
+            // Слово содержит число
+            return true;
+        }
+    }
+    // Слово не содержит чисел
+    return false;
+}
+
+unsigned int f1(const char* str) {
+    int count = 0;
+    std::vector<std::string> push_words;
+    push_words = Return_replicated_words(str);
+    for (const auto& word : push_words) {
+        if (checker_contain_number(word) == false) {
+            count++;
+        }
+    }
+    return count;
+}
+
+bool Сontain_num(const char item) {
+    if (item >= '0' && item <= '9') {
+        return true;
+    } else {
+        // Слово не содержит чисел
+        return false;
+    }
+}
+
+bool Null(const char item) {
+    if (' ' == item || item == '\0') {
+        return true;
+    }
+    return false;
+}
 
 unsigned int faStr1(const char* str) {
-    int i = 0;
-    bool inWord = false;
-    bool Have_Numbers = false;
-    int Count_Words = 0;
-    while (str[i] != '\0') {
-        if (isdigit(str[i]) && Have_Numbers == false) {
-            Have_Numbers = true;
+    bool read = false;
+    int allo = -1; allo++;
+    bool skip = false;
+    int sk = -1; sk++;
+    while (!('\0' == str[sk])) {
+        bool ex = Сontain_num(str[sk]);
+        if (!(skip == false)
+            && ex == true) {
+            sk++;
+            continue;
+        } else if (skip == false
+            && ex == true) {
+            skip = true;
         }
-        if (str[i] != ' ' && inWord == false) {
-            inWord = true;
-            Count_Words++;
-        } else if (str[i] == ' ' && inWord == true) {
-            if (Have_Numbers == true) {
-                Have_Numbers = false;
-                Count_Words--;
+        if (skip == false
+            && read == true && Null(str[sk])) {
+            read = false; skip = false;
+        } else if (!(skip == false)
+            && read == true && Null(str[sk])) {
+            read = false;
+            allo--; skip = false;
+        } else if (!(read == true)
+            && !(Null(str[sk]))) {
+            allo++; read = true;
+        }
+        sk++;
+    }
+    if (Null(str[sk])
+        && read == true && skip == true) {
+        allo--; skip = false;
+        read = false;
+    }
+    return allo;
+}
+
+bool checker_big_first_letter(std::string word) {
+    return word.at(0) >= 'A' && word.at(0) <= 'Z';
+}
+
+bool checker_little_letters(std::string word) {
+    bool flag_contain = false;
+    // Начинаем с второго символа
+    for (size_t i = 1; i < word.size(); ++i) {
+        if (word[i] >= 'a' && word[i] <= 'z') {
+            flag_contain = true;
+        }
+        else {
+            flag_contain = false;
+            break;
+        }
+    }
+    return flag_contain;
+}
+
+unsigned int f2(const char* str) {
+    std::vector<std::string> push_words;
+    push_words = Return_replicated_words(str);
+    bool Contain_big_letter = false;
+    bool Contain_little_letters = false;
+    int count_right_words = 0;
+    // Вывод результатов
+    for (const auto& word : push_words) {
+        Contain_big_letter
+            = checker_big_first_letter(word);
+        Contain_little_letters
+            = checker_little_letters(word);
+        if (word.length() == 1
+            && Contain_big_letter == true) {
+            count_right_words++;
+        }
+        if (Contain_big_letter == false) {
+            continue;
+        }
+        else {
+            if (Contain_little_letters == true) {
+                count_right_words++;
             }
-            inWord = false;
+            else {
+                continue;
+            }
         }
-        i++;
     }
-    if (inWord == true && Have_Numbers == true) {
-        Count_Words--;
-        Have_Numbers = false;
-    }
-    return Count_Words;
+    return count_right_words;
+}
+
+bool LOW(const char word) {
+    return word >= 'a' && word <= 'z';
+}
+
+bool UP(const char word) {
+    return word >= 'A' && word <= 'Z';
 }
 
 unsigned int faStr2(const char* str) {
-    int Count_Words = 0;
-    bool inW = false;
-    int i = 0;
-    bool NoL = false;
-    while (str[i] != '\0') {
-        if (str[i] != ' ' && inW == false && isupper(str[i])) {
-            inW = true;
-            Count_Words++;
-        } else if (str[i] != ' ' && inW == true && !islower(str[i])) {
-            if (NoL == false) {
-                NoL = true;
+    bool read = false;
+    //кол-во слов
+    int hallo = -1; hallo++;
+    bool skip = false;
+    int sk = -1; sk++;
+    while (str[sk] != '\0') {
+        if (Null(str[sk])
+            && read == true
+            && skip == true) {
+            read = false;
+            hallo--;
+            skip = false;
+        } else if (Null(str[sk])
+            && read == true
+            && skip == false) {
+            read = false;
+        }        
+        else if (!Null(str[sk])
+            && !LOW(str[sk]) 
+            && read == true) {
+            if (skip == false) {
+                skip = true;
             }
-        } else if (str[i] == ' ' && inW == true) {
-            if (NoL == true) {
-                Count_Words--;
-                NoL = false;
-            }
-            inW = false;
+        } else if (!Null(str[sk])
+            && UP(str[sk])
+            && read == false) {
+            hallo++;
+            read = true;
         }
-        i++;
+        sk++;
     }
-    return Count_Words;
+    return hallo;
 }
 
 unsigned int faStr3(const char* str) {
-    bool inWord = false;
-    double Count_Length = 0;
-    double Count_Words = 0;
-    int i = 0;
-    while (str[i] != '\0') {
-        if (str[i] != ' ' && inWord == false) {
-            inWord = true;
-            Count_Words++;
-            Count_Length++;
-        } else if (str[i] ==' ' && inWord == true) {
-            inWord = false;
-        } else if (str[i] != ' ' && inWord == true) {
-            Count_Length++;
-            inWord = true;
+    bool read = false;
+    //длина
+    int hallo = -1; hallo++;
+    //кол-во слов
+    int allo = -1; allo++;
+    int sk = -1; sk++;
+    while (!('\0' == str[sk])) {
+        if (Null(str[sk])
+            && read == true) {
+            read = false;
+            sk++;
+            continue;
+        } else if (Null(str[sk])
+            && read == false) {
+            sk++;
+            continue;
+        } else if (!Null(str[sk])
+            && read == false) {
+            read = true;
+            allo++;
+            hallo++;
+            sk++;
+            continue;
+        } else if (!Null(str[sk])
+            && read == true) {
+            hallo++;
+            sk++;
+            continue;
         }
-        i++;
     }
-    int Average_Value = round(Count_Length / Count_Words);
-    return Average_Value;
+    return hallo / allo;
 }
